1ebf48266faed841d4d7c36a40372af048e0c47d
erikgoe <erikgoe@users.noreply.github.com>
2021-01-24 19:04:07 +0100

    Add torus creation

--- terminalDonut.cpp
+++ terminalDonut.cpp
@@ -4,8 +4,11 @@
 struct Vector3 {
     float x = 0.f, y = 0.f, z = 0.f;
 
+    Vector3() {}
     Vector3( float x_val, float y_val, float z_val ) : x( x_val ), y( y_val ), z( z_val ) {}
     Vector3( const float arr[3] ) : x( arr[0] ), y( arr[1] ), z( arr[2] ) {}
+
+    static Vector3 in_xy_circle( float radians ) { return Vector3{ std::cos( radians ), std::sin( radians ), 0.f }; }
 
     Vector3 operator+( const Vector3 &other ) const { return Vector3{ x + other.x, y + other.y, z + other.z }; }
     Vector3 operator-( const Vector3 &other ) const { return Vector3{ x - other.x, y - other.y, z - other.z }; }
@@ -40,7 +43,25 @@ struct Matrix3 {
 };
 
 int main() {
-    auto m = Matrix3::rotationAroundX( M_PI );
-    std::cout << m.val[1][0] << " " << m.val[1][1] << " " << m.val[1][2] << std::endl;
+    // Create a simple ring in the 2d plane
+    const size_t RING_SECTIONS = 16;
+    Vector3 basic_ring[RING_SECTIONS];
+    for ( size_t i = 0; i < RING_SECTIONS; i++ )
+        basic_ring[i] = Vector3::in_xy_circle( i * M_PI * 2 / RING_SECTIONS );
+
+    // Create the torus
+    const size_t TORUS_SECTIONS = 32;
+    Vector3 torus[RING_SECTIONS * TORUS_SECTIONS];
+    Vector3 normals[RING_SECTIONS * TORUS_SECTIONS];
+    Vector3 index = Vector3{ 4.f, 0.f, 0.f };
+    for ( size_t i = 0; i < TORUS_SECTIONS; i++ ) {
+        auto rotation = Matrix3::rotationAroundY( i * M_PI / TORUS_SECTIONS );
+        for ( size_t j = 0; j < RING_SECTIONS; j++ ) {
+            normals[i * RING_SECTIONS + j] = rotation * basic_ring[i];
+            torus[i * RING_SECTIONS + j] = rotation * ( index + basic_ring[i] );
+        }
+    }
+
+
     return 0;
 }
